c1d156d73058cc9e47fcdcabc3bbee296758d015
rushLovenshier <ruwandi.2001.18@gmail.com>
2025-04-20 13:34:24 +0530

    country end points

---
+++ routes/countryRoutes.js
@@ -0,0 +1,14 @@
+// routes/countries.js
+import { Router } from "express";
+const router = Router();
+import { authenticateApiKey } from "../middleware/APIKey.js";
+import {
+  getAllCountries,
+  getCountryByName,
+} from "../controllers/restCountryController.js";
+
+router.get("/", authenticateApiKey, getAllCountries);
+
+router.get("/:name", authenticateApiKey, getCountryByName);
+
+export default router;
