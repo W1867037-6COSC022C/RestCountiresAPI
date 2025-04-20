f5e50c4590f7cd8a08923209e5e42022705d5595
rushLovenshier <ruwandi.2001.18@gmail.com>
2025-04-20 13:41:14 +0530

    Updating app.js with all routes and CORS

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ import express from "express";
 import pkg from "body-parser";
 const { json } = pkg;
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoute from "./routes/authRoutes.js";
+import apiKeyRoute from "./routes/apiKeyRoutes.js";
+import adminApiKeyRoute from "./routes/adminAPIKeyRoutes.js";
+import countryRoute from "./routes/countryRoutes.js";
 
 const app = express();
 
 app.use(json());
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 app.use("/auth", authRoute);
+app.use("/api-keys", apiKeyRoute);
+app.use("/admin/api-keys", adminApiKeyRoute);
+app.use("/countries", countryRoute);
 
 const PORT = process.env.PORT;
 
