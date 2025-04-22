d60ee1200a0bb62d72d9d3587994f9e5964f3a56
rushLovenshier <ruwandi.2001.18@gmail.com>
2025-04-22 08:02:44 +0530

    adding swagger

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ import pkg from "body-parser";
 const { json } = pkg;
 import cors from "cors";
 
+import path from "path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const swaggerDocument = YAML.load(join(__dirname, "openapi.yaml"));
+
 import authRoute from "./routes/authRoutes.js";
 import apiKeyRoute from "./routes/apiKeyRoutes.js";
 import adminApiKeyRoute from "./routes/adminAPIKeyRoutes.js";
 import countryRoute from "./routes/countryRoutes.js";
 
 const app = express();
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    explorer: true,
+  })
+);
 
 app.use(json());
 
